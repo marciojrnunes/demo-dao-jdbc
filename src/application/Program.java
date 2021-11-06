b99899e0f46bcdd69f5f9ece16c6feb683e74f56
Marcio Nunes <marcio-junior@hotmail.com>
2021-11-06 08:19:27 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! = New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);  //pega o vendedor de id = 1
+		seller.setName("MArtha Waine");  //Atribui um novo nome
+		sellerDao.update(seller);  //Atualiza os dados desse vendedor
+		System.out.println("Update Completed");
+		
 	}
 
 }
